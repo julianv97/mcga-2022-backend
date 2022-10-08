046fa3bdf999c122a362c1bfb081eb112a330480
julianv97 <julian.vicente@radiumrocket.com>
2022-10-08 16:47:25 -0300

    create activa product endpoint

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -4,8 +4,8 @@ export const getProducts = async (req, res) => {
   try {
     const products = await Product.find({ isDeleted: false });
     res.status(200).json(products);
-  } catch (err) {
-    res.status(500).send(err);
+  } catch (error) {
+    res.status(500).send({ msg: error });
   }
 };
 
@@ -14,8 +14,8 @@ export const getProductById = async (req, res) => {
     const product = await Product.findById(req.params.id);
     if (!product) return res.status(404).send({ message: 'Product not found' });
     res.status(200).json(product);
-  } catch (err) {
-    res.status(500).send(err);
+  } catch (error) {
+    res.status(500).send({ msg: error });
   }
 };
 
@@ -24,8 +24,8 @@ export const addProduct = async (req, res) => {
   try {
     await newProduct.save();
     res.status(201).json(newProduct);
-  } catch (err) {
-    res.status(500).send(err);
+  } catch (error) {
+    res.status(500).send({ msg: error });
   }
 };
 
@@ -35,7 +35,18 @@ export const deleteProduct = async (req, res) => {
     if (!product) return res.status(404).send('No product found');
     if (product.isDeleted) return res.status(404).json({ message: 'Product already deleted' });
     res.status(200).json({ message: 'Product deleted' });
-  } catch (err) {
-    res.status(500).send(err);
+  } catch (error) {
+    res.status(500).send({ msg: error });
+  }
+};
+
+export const activateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, { isDeleted: false });
+    if (!product) return res.status(404).send('No product found');
+    if (!product.isDeleted) return res.status(404).json({ message: 'Product is not deleted' });
+    res.status(200).json({ message: 'Product activated' });
+  } catch (error) {
+    res.status(500).send({ msg: error });
   }
 };
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import {
-  getProducts, addProduct, deleteProduct, getProductById,
+  getProducts, addProduct, deleteProduct, getProductById, activateProduct,
 } from '../controllers/products.js';
 
 const router = express.Router();
 
 router.get('/', getProducts);
+router.get('/:id', getProductById);
 router.post('/add', addProduct);
 router.delete('/:id', deleteProduct);
-router.get('/:id', getProductById);
+router.put('/:id', activateProduct);
 
 export default router;
