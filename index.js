3947aa832054e2081ad1cf6360ead7ec93b987ee
julianv97 <julian.vicente@radiumrocket.com>
2022-10-18 18:29:25 -0300

    add cors support

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.use(router);
